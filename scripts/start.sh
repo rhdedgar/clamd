fa42e631e0272627072a956e5c7755bc89328aac
Doug Edgar <dedgar@redhat.com>
2022-03-09 11:46:03 -0800

    testing as part of the signature-updatee deployment

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-echo "clamd container v0.0.3"
+echo "clamd container v0.0.5"
 
 # This is useful so we can debug containers running inside of OpenShift that are
 # failing to start properly.
@@ -14,9 +14,20 @@ if [ "$OO_PAUSE_ON_START" = "true" ] ; then
   done
 fi
 
+if [ "$UPDATE_ONLY" = "true" ] ; then
+  echo
+  echo "The UPDATE_ONLY variable has been set. This container will attempt to keep the pod's shared volume updated with new clam DBs without starting clamd."
+  echo
+  echo 'Updating ClamAV official and unofficial signatures every 12 hours'
+  echo '----------------'
+    /usr/local/bin/ops-run-in-loop 43200 /usr/bin/freshclam 2>&1 &
+    sleep 300
+    /usr/local/bin/ops-run-in-loop 43200 /usr/bin/clamav-unofficial-sigs.sh
+fi
+
 echo This container hosts the following applications:
 echo
 echo '/usr/sbin/clamd'
 echo
-echo 'start clamd in the foreground so we can easily check status with oc logs'
+echo 'Start clamd in the foreground so we can easily check status with oc logs'
 /usr/sbin/clamd -c /etc/clamd.d/scan.conf --foreground=yes
